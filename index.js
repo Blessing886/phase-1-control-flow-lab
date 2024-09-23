3a8e3128ed1c8475994c2226c1118f1256e30733
BlessingOkora <moraablessing082@gmail.com>
2024-09-23 22:13:01 +0300

    Adds index.html

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return "This one is on me!"; 
+  } 
+  else if(rideDistance > 400 && rideDistance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if(rideDistance > 2000 && rideDistance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if(rideDistance > 2500) {
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType) {
+  if (tipType === "generous") {
+    return "Thank you so much.";
+  }
+  else if(tipType === "not as generous") {
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
 }
